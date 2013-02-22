98b4e1d8c1726598705fff08fa88373cb7acdf1a
alessio ricco <alessio.ricco@gmail.com>
2013-02-22 01:14:57 +0100

    modifica a cenvironment

--- Resources/ui/common/environment.js
+++ Resources/ui/common/environment.js
@@ -1,4 +1,4 @@
-exports.BEINTOOAPI =  PUT_YOUR_APIKEY; // http://www.beintoo.com/business/developer/app.html;
+exports.BEINTOOAPI =  "ec9aebd26210dd576dad135413acabe5"; //PUT_YOUR_APIKEY; // http://www.beintoo.com/business/developer/app.html;
 exports.USE_SANDBOX = true;
 
 exports.userEmail = "ticonf@mailcatch.com";
